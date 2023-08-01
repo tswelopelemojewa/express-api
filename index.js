a1a7d4c9c0777d338a7038a101f7fa84032b9267
tswelopelemojewa <tswelopelemojewa@gmail.com>
2023-08-01 19:30:04 +0200

    first api

--- index.js
+++ index.js
@@ -2,33 +2,43 @@ import express from 'express';
 
 const app = express();
 
+//declare configurable port number
+const PORT = process.env.PORT || 4009;
+
+
 // app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
 
 // add the line below - and a public folder containing and index.html file
 app.use(express.static('public'));
 
+
 const greetings = {
     'english' : 'Hello',
 }
 
-// call the api like this - http://localhost:4009/api/greet?username=Andre
+
+// call the api like this - http://localhost:4009/api/greet?username=tswelopele
 app.get('/api/greet', function(req, res){
-    // console.log(req.query);
+
+    //req is used to send requests - request
     const username = req.query.username;
     const language = req.query.language;
     
     if (!greetings[language]) {
         return res.json({
-            error : 'Invalid language supplied : ' + language
+            error : `Invalid language supplied :  ${language}`
         })
     }
 
     const greeting = greetings[language];
+
+    // res is used to display back - response/result
     res.json({
         message : `${greeting}, ${username}!`
     })
 });
+
 
 app.post('/api/greet', function(req, res){
     
@@ -43,6 +53,10 @@ app.post('/api/greet', function(req, res){
 
 });
 
+
+
+
+
 app.get('/api/greet/:username', function(req, res){
     console.log(req.params);
     const username = req.params.username;
@@ -51,7 +65,7 @@ app.get('/api/greet/:username', function(req, res){
     })
 });
 
-const PORT = process.env.PORT || 4009;
+
 
 app.listen(PORT, function(){
     console.log(`app started on port ${PORT}`)
